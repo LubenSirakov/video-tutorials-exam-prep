de6531704df900d0e748ad918ed9bedf45120900
Luben Sirakov <lubo95sirakov@gmail.com>
2021-10-26 19:45:27 +0300

    add basic User model

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    // enrolledCourses: {
+        
+    // }
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
