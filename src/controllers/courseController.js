3bf4dfd6cc5bd50e1425be5c4eed2dc6a9536b20
Luben Sirakov <lubo95sirakov@gmail.com>
2021-10-28 12:20:59 +0300

    add edit and delete  course functionality

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -3,12 +3,7 @@ const router = require('express').Router();
 const courseService = require('../services/courseService.js');
 const { isAuth } = require('../middlewares/authMiddleware.js');
 
-router.get('/', async (req, res) => {
-    let courses = await courseService.getAll();
-
-    res.render('home', { courses });
-});
-//CREATE PAGE
+//---CREATE PAGE---
 router.get('/create', isAuth, (req, res) => {
     res.render('course/create');
 });
@@ -20,25 +15,79 @@ router.post('/create', isAuth, async (req, res) => {
 
         res.redirect('/');
     } catch (error) {
-        
+
         res.render('course/create')
     }
 });
-//DETAILS PAGE
+
+//---DETAILS PAGE---
 router.get('/:courseId/details', async (req, res) => {
     let course = await courseService.getOne(req.params.courseId);
     let courseData = await course.toObject();
-    console.log(courseData);
-    res.render('course/details', {...courseData});
+
+    let isCreator = courseData.creator == req.user?._id;
+
+    res.render('course/details', { ...courseData, isCreator });
 });
 
-async function isCreator(erq, res, next) {
-    let course = await courseService.getOne(req.params.courseId);
+//---DELETE COURSE---
+router.get('/:courseId/delete', notCreator, async (req, res) => {
+    try {
+        let courseId = req.params.courseId;
+    
+        await courseService.delete(courseId);
+    
+        res.redirect('/');
+        
+    } catch (error) {
+        console.log(error);
+    }
+})
 
-    if(course.creator == res.user._id) {
+//---EDIT PAGE---
+router.get('/:courseId/edit', notCreator, async (req, res) => {
+    try {
+        let course = await courseService.getOne(req.params.courseId);
+        let courseData = await course.toObject();
+    
+        res.render('course/edit', { ...courseData });
+        
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.post('/:courseId/edit', async (req, res) => {
+    try {
+        let courseId = req.params.courseId;
+        let courseData = req.body;
+    
+        await courseService.updateOne(courseId, courseData);
+    
+        res.redirect(`/course/${courseId}/details`);
+        
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+async function isCreator(req, res, next) {
+    let course = await courseService.getOne(req.params.courseId);
+    
+    if (course.creator == req.user._id) {
         res.redirect(`course/${req.params.courseId}/details`);
     } else {
         next();
+    }
+};
+
+async function notCreator(req, res, next) {
+    let course = await courseService.getOne(req.params.courseId);
+    
+    if (course.creator !== req.user._id) {
+        next();
+    } else {
+        res.redirect(`/course/${req.params.courseId}/details`)
     }
 }
 
--- src/services/courseService.js
+++ src/services/courseService.js
@@ -5,3 +5,7 @@ exports.getAll = () => Course.find({}).lean();
 exports.getOne = (courseId) => Course.findById(courseId).populate('usersEnrolled');
 
 exports.create = (courseData) => Course.create(courseData);
+
+exports.updateOne = (courseId, courseData) => Course.findByIdAndUpdate(courseId, courseData);
+
+exports.delete = (courseId) => Course.findByIdAndDelete(courseId);
